2ab2b15ca93ba36e5dc5527343ab4f76e3e43ebc
zylkowski <arkadiuszzylkowski@gmail.com>
2021-05-29 15:40:57 +0200

    Implemented builder for sim manager

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,50 @@
-
-pub struct SimManger{
-
+pub struct SimManger<DynamicParameters, EnvironmentParameters> {
+    parameters: Vec<DynamicParameters>,
+    environment_parameters: EnvironmentParameters,
 }
-impl SimManger{
-    fn with_initial_conditions(initial_conditions: DynConditions) -> &Self {
-        todo!()
-    } 
+impl<DynamicParameters, EnvironmentParameters> SimManger<DynamicParameters, EnvironmentParameters> {
+    fn with_initial_conditions(
+        initial_conditions: DynamicParameters,
+        environment_parameters: EnvironmentParameters,
+    ) -> Self {
+        SimManger {
+            parameters: vec![initial_conditions],
+            environment_parameters,
+        }
+    }
 
-    fn with_step(step_f: FnMut(&DynConditions,&mut StaticConditions) -> DynConditions) {
+    fn with_step<StepF>(&self, step_f: StepF)
+    where
+        StepF: FnMut(&DynamicParameters, &mut EnvironmentParameters) -> DynamicParameters,
+    {
         todo!()
     }
 }
 
-
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn test_1(){
+    fn building() {
+        struct DynamicParameters {
+            x: f32,
+            y: f32,
+            v_x: f32,
+            v_y: f32,
+        }
+        struct EnvironmentParameters {
+            g: f32,
+        }
 
+        SimManger::with_initial_conditions(
+            DynamicParameters {
+                x: 0.,
+                y: 0.,
+                v_x: 0.,
+                v_y: 0.,
+            },
+            EnvironmentParameters { g: 9.87 },
+        );
     }
 }
