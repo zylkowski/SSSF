3806ebbe4278f26724e6df82a2381f5da5154208
zylkowski <arkadiuszzylkowski@gmail.com>
2021-05-29 23:22:23 +0200

    Added stop simulation feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,32 @@
-pub struct SimManger<DynamicParameters, EnvironmentParameters> {
+pub struct SimManger<DynamicParameters, EnvironmentParameters, StepFn, StopFn>
+where
+    StepFn: FnMut(&DynamicParameters, &mut EnvironmentParameters) -> DynamicParameters,
+    StopFn: FnMut(&DynamicParameters, &EnvironmentParameters) -> bool,
+{
     parameters: Vec<DynamicParameters>,
     environment_parameters: EnvironmentParameters,
+    step_fn: StepFn,
+    stop_fn: StopFn,
 }
-impl<DynamicParameters, EnvironmentParameters> SimManger<DynamicParameters, EnvironmentParameters> {
-    fn with_initial_conditions(
+
+impl<DynamicParameters, EnvironmentParameters, StepFn, StopFn>
+    SimManger<DynamicParameters, EnvironmentParameters, StepFn, StopFn>
+where
+    StepFn: FnMut(&DynamicParameters, &mut EnvironmentParameters) -> DynamicParameters,
+    StopFn: FnMut(&DynamicParameters, &EnvironmentParameters) -> bool,
+{
+    fn new(
         initial_conditions: DynamicParameters,
         environment_parameters: EnvironmentParameters,
+        step_fn: StepFn,
+        stop_fn: StopFn,
     ) -> Self {
         SimManger {
             parameters: vec![initial_conditions],
             environment_parameters,
+            step_fn,
+            stop_fn,
         }
-    }
-
-    fn with_step<StepF>(&self, step_f: StepF)
-    where
-        StepF: FnMut(&DynamicParameters, &mut EnvironmentParameters) -> DynamicParameters,
-    {
-        todo!()
     }
 }
 
@@ -28,6 +37,7 @@ mod tests {
     #[test]
     fn building() {
         struct DynamicParameters {
+            t: f32,
             x: f32,
             y: f32,
             v_x: f32,
@@ -35,16 +45,26 @@ mod tests {
         }
         struct EnvironmentParameters {
             g: f32,
+            dt: f32,
         }
 
-        SimManger::with_initial_conditions(
+        SimManger::new(
             DynamicParameters {
+                t: 0.,
                 x: 0.,
                 y: 0.,
                 v_x: 0.,
                 v_y: 0.,
             },
-            EnvironmentParameters { g: 9.87 },
+            EnvironmentParameters { g: 9.87, dt: 0.1 },
+            |dyn_paramteres, environment_parameters| DynamicParameters {
+                t: dyn_paramteres.t + environment_parameters.dt,
+                x: dyn_paramteres.x,
+                y: dyn_paramteres.y,
+                v_x: dyn_paramteres.v_x,
+                v_y: dyn_paramteres.v_y,
+            },
+            |dyn_paramteres, environment_parameters| dyn_paramteres.t == 100,
         );
     }
 }
