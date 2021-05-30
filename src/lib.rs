949969c26116329b6d3e9b6665f306c7f5c9e045
zylkowski <arkadiuszzylkowski@gmail.com>
2021-05-30 12:49:03 +0200

    Created separate file for SimManager

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,4 @@
-pub struct SimManger<DynamicParameters, EnvironmentParameters, StepFn>
-where
-    StepFn: FnMut(&DynamicParameters, &mut EnvironmentParameters) -> DynamicParameters,
-{
-    parameters: Vec<DynamicParameters>,
-    environment_parameters: EnvironmentParameters,
-    step_fn: StepFn,
-}
 
-impl<DynamicParameters, EnvironmentParameters, StepFn>
-    SimManger<DynamicParameters, EnvironmentParameters, StepFn>
-where
-    StepFn: FnMut(&DynamicParameters, &mut EnvironmentParameters) -> DynamicParameters,
-{
-    fn new(
-        initial_conditions: DynamicParameters,
-        environment_parameters: EnvironmentParameters,
-        step_fn: StepFn,
-    ) -> Self {
-        SimManger {
-            parameters: vec![initial_conditions],
-            environment_parameters,
-            step_fn,
-        }
-    }
-
-    fn run<StopFn>(&mut self, stop_fn: StopFn)
-    where
-        StopFn: Fn(&DynamicParameters, &EnvironmentParameters) -> bool,
-    {
-        loop {
-            let new_parameters = (self.step_fn)(
-                self.parameters.last().unwrap(),
-                &mut self.environment_parameters,
-            );
-            self.parameters.push(new_parameters);
-
-            if stop_fn(
-                self.parameters.last().unwrap(),
-                &self.environment_parameters,
-            ) {
-                break;
-            }
-        }
-    }
-}
 
 #[cfg(test)]
 mod tests {
